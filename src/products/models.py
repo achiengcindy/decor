7ba87843424b8a067e537fe460145b56cfca944b
Cindy Muga <achiengcindy36@gmail.com>
2019-01-22 12:36:52 +0300

    Created the product catalog
    created the catalog and categories in model.I went ahead and registered thein admin

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True, help_text='Unique value for product page URL, created from name.')
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+    meta_description = models.CharField("Meta Description", max_length=255, help_text='Content for description meta tag')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'categories'
+        ordering = ('-created',)
+        verbose_name = 'category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('products:product_category',args=[self.slug])
+
+
+
+
+class Product(models.Model):
+    """ model class containing information about a product; instances of this class are what the user
+    adds to their shopping cart and can subsequently purchase
+    
+    """
+    name = models.CharField(max_length=190, db_index=True)
+    slug = models.SlugField(max_length=190, db_index=True)
+    brand = models.CharField(max_length=50)
+    sku = models.CharField(max_length=50)
+
+    price = models.DecimalField(max_digits=9,decimal_places=2)
+    old_price = models.DecimalField(max_digits=9,decimal_places=2, blank=True,default=0.00)
+
+    # image fields
+    image = models.ImageField(upload_to='images/products/main/%Y/%m/%d',blank=True)
+    image_caption = models.CharField(max_length=200)
+
+    is_active = models.BooleanField(default=True)
+    is_bestseller = models.BooleanField(default=False)
+    is_featured = models.BooleanField(default=False)
+    available = models.BooleanField(default=True)
+    quantity = models.PositiveIntegerField()
+    description = models.TextField()
+
+    meta_keywords = models.CharField(max_length=255, help_text='Comma-delimited set of SEO keywords for meta tag')
+    meta_description = models.CharField(max_length=255, help_text='Content for description meta tag')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField(Category, related_name='products')
+
+    class Meta:
+        db_table = 'products'
+        ordering = ('-created',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('products:product_detail',args=[self.id, self.slug])
+     
+    def sale_price(self):
+        if self.old_price > self.price:
+            return self.price
+        else:
+            return None
+
+    
+
