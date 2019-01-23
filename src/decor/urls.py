371b7c26c1fe3222df2355b9418fa123c2d1ac66
Cindy Muga <achiengcindy36@gmail.com>
2019-01-23 13:20:46 +0300

    Created Privacy policy and about us flatpages

--- src/decor/urls.py
+++ src/decor/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('', include('products.urls' , namespace='products')),
+    path('pages/', include('django.contrib.flatpages.urls')),
 ]
 
 if settings.DEBUG:
