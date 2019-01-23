5e7f9ec64fc29090ef5ebde0d3d0a0f08eea2960
Cindy Muga <achiengcindy36@gmail.com>
2019-01-23 13:28:26 +0300

    Included flatpages in footer

--- src/products/templatetags/products_filters.py
+++ src/products/templatetags/products_filters.py
@@ -1,5 +1,4 @@
 from django import template
-from products.models import Category
 import locale
 from decimal import Decimal
 
@@ -14,7 +13,3 @@ def currency(value):
     loc = locale.localeconv()
     return locale.currency(value, loc['currency_symbol'], grouping=True)
 
-@register.inclusion_tag("tags/category_list.html")
-def category_list(request_path):
-    active_categories = Category.objects.filter(is_active=True)
-    return {'active_categories': active_categories,'request_path': request_path}
---
+++ src/products/templatetags/products_tags.py
@@ -0,0 +1,19 @@
+from ..models import Category
+from django import template
+from django.contrib.flatpages.models import FlatPage
+
+register = template.Library()
+
+@register.inclusion_tag("tags/category_list.html")
+def category_list(request_path):
+    active_categories = Category.objects.filter(is_active=True)
+    return {'active_categories': active_categories,  'request_path': request_path}
+
+@register.inclusion_tag("tags/product_list.html")
+def product_list(products, header_text):
+    return { 'products': products,'header_text': header_text }
+
+@register.inclusion_tag("tags/footer.html")
+def footer_links():
+    flatpage_list = FlatPage.objects.all()
+    return {'flatpage_list': flatpage_list }
