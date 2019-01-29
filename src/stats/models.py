e5b003854e48113e1f3e25148653044e4d35d3d8
Cindy Muga <achiengcindy36@gmail.com>
2019-01-29 09:18:45 +0300

    view based recommendations

--- src/stats/models.py
+++ src/stats/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+class PageView(models.Model):
+    """ model class for tracking the pages that a customer views """
+    class Meta:
+        abstract = True
+        
+    date = models.DateTimeField(auto_now=True)
+    ip_address = models.GenericIPAddressField()
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    tracking_id = models.CharField(max_length=50, default='', db_index=True)
+
+class ProductView(PageView):
+    """ tracks product pages that customer views """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,)
