3b3f587a5fc4138aa7a203a33dd59e15b78c7b69
Cindy Muga <achiengcindy36@gmail.com>
2019-02-02 08:56:15 +0300

    Registration Form

---
+++ src/accounts/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+User= get_user_model()
+
+class RegistrationForm(forms.ModelForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput,  help_text='Password must be six characters long')
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput,  help_text='Password must be six characters long')
+
+    class Meta:
+        model = User
+        fields = ('username','email','first_name',) 
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Please use another Email,that is already taken')
+        return email
