ad42dc9b67f914c430ca7574b2aea8dd582cbfca
Cindy Muga <achiengcindy36@gmail.com>
2019-02-04 09:13:23 +0300

    registration templates

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .forms import RegistrationForm
+User= get_user_model()
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = RegistrationForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save(commit=False)
+            user.email = user_form.cleaned_data['email']
+            user.set_password(user_form.cleaned_data['password'])
+            user.is_active = False
+            # Save the User
+            # object
+            user.save()
+            return redirect('/')
+            
+
+    else:
+        user_form = RegistrationForm()
+    return render(request, 'accounts/register.html',{'user_form': user_form})
+
