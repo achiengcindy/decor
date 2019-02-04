39654dfa0a0154f3431051b655d6787ddeda659c
Cindy Muga <achiengcindy36@gmail.com>
2019-02-04 10:37:25 +0300

    Activating User only after confirming the tokens

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render , redirect, get_object_or_404
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.http import HttpResponse
+from django.contrib import messages
 
-from .forms import RegistrationForm
+from checkout.models import Order, OrderItem
+from .forms import RegistrationForm, ProfileEditForm, UserEditForm
+from .tokens import account_activation_token
+from .models import Profile
+
 User= get_user_model()
 
 
@@ -12,14 +23,49 @@ def register(request):
             user = user_form.save(commit=False)
             user.email = user_form.cleaned_data['email']
             user.set_password(user_form.cleaned_data['password'])
+            #user becomes active only after activating tokens
             user.is_active = False
-            # Save the User
-            # object
+            # Save the User object
             user.save()
-            return redirect('/')
-            
-
+            #send one time activation email
+            current_site = get_current_site(request)
+            subject = 'Account Confirmation'
+            sender = settings.DEFAULT_FROM_EMAIL
+            message = render_to_string('accounts/account_activation_email.html', {
+                'user':  user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes( user.pk)).decode(),
+                'token':account_activation_token.make_token( user),
+            })
+            print( message)
+            # https://stackoverflow.com/questions/40655802/how-to-implement-email-user-method-in-custom-user-model
+            # email = EmailMessage(subject, message,sender, [user.email])
+            # print(email)
+            # email.send()
+            user.email_user(subject=subject, message=message)
+            return HttpResponse('Check your Email,tokens sent succesfully')
+  
     else:
         user_form = RegistrationForm()
     return render(request, 'accounts/register.html',{'user_form': user_form})
+
+def activate(request, uidb64, token, backend='accounts.authentication.EmailAuthBackend'):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError,User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user, 'accounts.authentication.EmailAuthBackend')
+        return redirect('login')
+    else:
+        return render(request, 'accounts/account_activation_invalid.html')
+
+
+ 
+
+
+
 
