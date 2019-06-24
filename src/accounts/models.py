9a5b0fa5a2aa4bbd56d208457827bd18035af135
cindy <achiengcindy36@gmail.com>
2019-06-24 14:13:21 +0300

    formatting the clean_phone and clean_price methods
    Adjusted the indentation

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -6,27 +6,29 @@ from checkout.models import OrderDetails
 
 User = get_user_model()
 
+
 class Profile(OrderDetails):
-	user = models.OneToOneField(User, on_delete=models.CASCADE,)
-	birth_date = models.DateField(blank=True, null=True, help_text='Please use the following format: YYYY-MM-DD.',) 
+    user = models.OneToOneField(User, on_delete=models.CASCADE,)
+    birth_date = models.DateField(
+        blank=True, null=True, help_text='Please use the following format: YYYY-MM-DD.',)
 
-	photo = models.ImageField(upload_to='users/%Y/%m/%d',blank=True)
-	# contact info
-	phone = models.CharField(max_length=20)
-	#shipping information
-	physical_address =  models.CharField(max_length=250)
-	postal_code = models.CharField(max_length=20)
-	city = models.CharField(max_length=100)
-	Estate = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+    # contact info
+    phone = models.CharField(max_length=20)
+    # shipping information
+    physical_address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    Estate = models.CharField(max_length=100)
+
+    def __str__(self):
+        return 'Profile of user: {}'.format(self.user.username)
 
 
-	def __str__(self):
-		return 'Profile of user: {}'.format(self.user.username)
-
-
-
-#create a user profile  when user is created
+# create a user profile  when user is created
 def create_user_profile(sender, instance, created, **kwargs):
-	if created:
-		profile = Profile.objects.get_or_create(user=instance)
+    if created:
+        profile = Profile.objects.get_or_create(user=instance)
+
+
 post_save.connect(create_user_profile, sender=User)
--- src/checkout/forms.py
+++ src/checkout/forms.py
@@ -2,18 +2,17 @@
 from django import forms
 from .models import Order
 
+
 class CheckoutForm(forms.ModelForm):
     class Meta:
         model = Order
-        exclude = ('owner','status','created','updated','Mpesa_transid','paid','discount',)
+        exclude = ('owner', 'status', 'created', 'updated',
+                   'Mpesa_transid', 'paid', 'discount',)
 
-        def clean_phone(self):
-            phone = self.cleaned_data['phone']
-            stripped_phone = strip_non_numbers(phone)
-            if len(stripped_phone) < 10:
-                raise forms.ValidationError('Enter a valid phone number.(e.g.07********)')
-            return self.cleaned_data['phone']
-
-
-
-
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+        stripped_phone = strip_non_numbers(phone)
+        if len(stripped_phone) < 10:
+            raise forms.ValidationError(
+                'Enter a valid phone number.(e.g.07********)')
+        return self.cleaned_data['phone']
