b547af73bdf70e26fc45f2537efe10fe3e521348
cindy <achiengcindy36@gmail.com>
2019-06-25 12:51:53 +0300

    inheriting from the profile model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,25 +1,21 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from checkout.models import OrderDetails
+# from checkout.models import OrderDetails
 
 User = get_user_model()
 
 
-class Profile(OrderDetails):
+# class Profile(OrderDetails):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,)
     birth_date = models.DateField(
         blank=True, null=True, help_text='Please use the following format: YYYY-MM-DD.',)
 
     photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
-    # contact info
-    phone = models.CharField(max_length=20)
-    # shipping information
-    physical_address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
-    Estate = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return 'Profile of user: {}'.format(self.user.username)
--- src/products/views.py
+++ src/products/views.py
@@ -3,22 +3,22 @@ from .models import Category, Product
 from django.core.paginator import Paginator
 from stats import stats
 from django.conf import settings
-from cart.forms import CartAddProductForm 
+from cart.forms import CartAddProductForm
 
 
 def product_list(request):
     page_title = 'Decor Products'
     search_recs = stats.recommended_from_search(request)
-    #default object manager
-    #products = Product.objects.filter(is_active=True)
-    #customized actve manager
+    # default object manager
+    # products = Product.objects.filter(is_active=True)
+    # customized actve manager
     new_arrivals = Product.objects.filter(is_new=True)
     products = Product.active.all()
     bestseller = Product.objects.filter(is_bestseller=True)
     featured = Product.featured.all()[0:settings.PRODUCTS_PER_ROW]
     recently_viewed = stats.get_recently_viewed(request)
     view_recs = stats.recommended_from_views(request)
-    return render(request,'product/list.html',{ 'products': products, 'bestseller':bestseller,'featured':featured,'search_recs':search_recs,'new':new_arrivals})
+    return render(request, 'product/list.html', {'products': products, 'bestseller': bestseller, 'featured': featured, 'search_recs': search_recs, 'new': new_arrivals})
 
 
 def list_category(request, slug):
@@ -26,7 +26,7 @@ def list_category(request, slug):
     products = category.product_set.all()
     print(products)
     meta_description = category.meta_description
-    return render(request, "product/category.html", {'category':category, 'products':products})
+    return render(request, "product/category.html", {'category': category, 'products': products})
 
 
 def product_detail(request, id, slug):
@@ -35,9 +35,4 @@ def product_detail(request, id, slug):
     cart_product_form = CartAddProductForm()
     meta_description = p.meta_description
     stats.log_product_view(request, p)
-    return render(request, "product/detail.html", {'c':c, 'p':p, 'cart_product_form': cart_product_form,})
-
-
-
-
-
+    return render(request, "product/detail.html", {'c': c, 'p': p, 'cart_product_form': cart_product_form, })
